027b11834a3b0d841f60d5e8b3e97ad1419212c6
Jlestat <jlestatt@gmail.com>
2023-09-05 22:47:14 +0300

    create views and controllers

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-
-from news.views import *
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', index),
-    path('test/', test),
+    path('news/', include('news.urls')),
 ]
