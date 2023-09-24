84aa5c35a0abab35cb496787e970d6fad8a17297
Jlestat <jlestatt@gmail.com>
2023-09-24 20:30:22 +0300

    implemented form output and error validation

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -3,7 +3,8 @@ from .models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField(max_length=150)
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, label='Название')
+    content = forms.CharField(max_length=150, label='Текст', required=False)
+    is_published = forms.BooleanField(label='Опубликовано', initial=True)
+    category = forms.ModelChoiceField(empty_label='Выберите категорию', label='Категория',
+                                      queryset=Category.objects.all())
