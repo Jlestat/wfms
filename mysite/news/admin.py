1979befb80143ceeacfc008e130304525593f188
Jlestat <jlestatt@gmail.com>
2023-10-16 18:51:32 +0300

    admin panel customization

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from news.models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'photo')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
-    list_editable = ('is_published', )
+    list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
-
+    fields = (
+        'id', 'title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
 
     def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width=65 height=65>')
+        else:
+            return '-'
 
+    get_photo.short_description = 'Миниатюра'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -22,4 +31,3 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
-
