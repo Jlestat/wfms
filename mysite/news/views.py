02455c8bd00fac97c858502408fbd8321c63b9a0
Jlestat <jlestatt@gmail.com>
2023-11-12 15:15:28 +0300

    add login and register

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -8,9 +8,20 @@ from .models import News, Category
 from .forms import NewsForm
 from .utils import MyMixin
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserCreationForm()
     form = UserCreationForm()
     return render(request, 'news/register.html', {'form': form})
 
