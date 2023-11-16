fe00503ad39229e24d62e4c39b6cf51efc1d136c
Jlestat <jlestatt@gmail.com>
2023-11-16 21:11:26 +0300

    smtp

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -133,3 +133,10 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INTERNAL_IPS = ["127.0.0.1"]
+
+EMAIL_HOST = "smtp.mail.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'tankistik9@mail.ru'
+EMAIL_HOST_PASSWORD = '9826327Say'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
